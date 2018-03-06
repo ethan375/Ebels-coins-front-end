02f50d34449580f2fa4fb6118be654f9abf8292f
ethan375 <ethanebel1@gmail.com>
2018-03-05 22:06:32 -0600

    routes setup

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 console.log("files connected");
 const app = angular.module("Coinsite", ['ngRoute']);
 
+Coinsite.config(['$routeProvider', function(routeProvider) {
+  $routeProvider
+  .when('/store', {
+    templateUrl: './views/store'
+  })
+  .when('/forum', {
+    templateUrl: './views/forum'
+  }).when('/info', {
+    templateUrl: './views/info',
+    // this is where were going to specify the controller for the view
+  }).otherwise({
+    redirectTo: '/store'
+  })
+}]);
+
 app.controller("StoreController", ['$http',function ($http){
+
+
 
 }]);
