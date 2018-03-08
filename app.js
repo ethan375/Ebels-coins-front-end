35ccec404e407bab4a32cb010abae9c9c850e1c9
ethan375 <ethanebel1@gmail.com>
2018-03-08 16:37:26 -0600

    setup routes for coin views, got ebay browse api working

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // console.log("files connected");
 const app = angular.module('Coinsite', ['ngRoute']);
 
+// app.config(['$httpProvider'function($httpProvider) {}])
+
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
   .when('/store', {
     templateUrl: './views/store.html',
-    controller: "StoreController" 
+    controller: "StoreController as str" 
   })
   .when('/forum', {
     templateUrl: './views/forum.html',
@@ -13,6 +15,49 @@ app.config(['$routeProvider', function($routeProvider) {
   }).when('/info', {
     templateUrl: './views/info.html',
     controller: "InfoController as inf"
+  }).when('/register', {
+    templateUrl: './views/register.html',
+    controller: 'RegisterController as rgs'
+  }).when('/halfcent', {
+    templateUrl: './views/'
+  }).when('/largeCent', {
+    templateUrl: './views/'
+  }).when('/smallCent', {
+    templateUrl: './views/'
+  }).when('/nickels', {
+    templateUrl: './views/'
+  }).when('/halfDimes', {
+    templateUrl: './views/'
+  }).when('/dimes', {
+    templateUrl: './views/'
+  }).when('/twentyCents', {
+    templateUrl: './views/'
+  }).when('/quarters', {
+    templateUrl: './views/'
+  }).when('/halfDollars', {
+    templateUrl: './views/'
+  }).when('/silverDollar', {
+    templateUrl: './views/'
+  }).when('/modernDollar', {
+    templateUrl: './views/'
+  }).when('/tradeDollar', {
+    templateUrl: './views/'
+  }).when('/goldDollar', {
+    templateUrl: './views/'
+  }).when('/goldQuarter', {
+    templateUrl: './views/'
+  }).when('/threeDollaGold', {
+    templateUrl: './views/'
+  }).when('/fourDollaGold', {
+    templateUrl: './views/'
+  }).when('/goldHalfEagle', {
+    templateUrl: './views/'
+  }).when('/tenGoldEagle', {
+    templateUrl: './views/'
+  }).when('/goldDoubleEagle', {
+    templateUrl: './views/'
+  }).when('/commerative', {
+    templateUrl: './views/'
   })
 }]);
 
@@ -23,14 +68,19 @@ app.controller("ForumController", ['$http', function ($http){
   this.login = function() {
     const makeAjaxCall = $http({
       method: 'POST',
-      url: 'localhost'
+      url: 'http://localhost:9292/users/login'
+    }).then(function(res) {
+      console.log(res)
+    }, function(err) {
+      console.error(err)
     })
   }
 
   this.getComments = function() {
     const makeAjaxCall = $http({
       method:'GET',
-      url: 'http://localhost:9292/comments'
+      url: 'http://localhost:9292/comments',
+
     }).then(function(res) {
       // console.log(res.data);
       controller.comments = res.data;
@@ -44,12 +94,19 @@ app.controller("ForumController", ['$http', function ($http){
 
 
 app.controller("StoreController", ['$http', function($http) {
+
+  controller = this;
+
   this.getEbayItems = function() {}
   const makeAjaxCall = $http({
     method: 'GET',
-    url: 'https://api.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3'
+    url: 'https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=108765&q=Beatles&filter=price:[200..500]&filter=priceCurrency:USD&limit=10',
+    headers:{
+      'Authorization': 'Bearer v^1.1#i^1#r^0#f^0#p^3#I^3#t^H4sIAAAAAAAAAOVXW2wUVRjutttiAy2RS1GCsAyWcMnsnrl2dsKu2d6ksbQrW8CWYHNm5kw7dHZmnTPbdnkwpSYYARMIJCRGa6OSyIMVjFoND0TjBQIxSEwUYjAxJOLlQQhRAwie2bbLtkagLQ9N3EyymXP+6/d/55/zg76S0jW71u/6s8w3q3CwD/QV+nzMbFBaUry2vKhwcXEByBPwDfY93ufvL7q8DsOkmZI3IpyyLYwCvUnTwnJ2MUKlHUu2ITawbMEkwrKryonYhkaZDQI55diurdomFWiojVC8xGoiJyEJcLzCKxpZtcZsttgRSkAa0AXAIMhzrIrINsZp1GBhF1puhGIBI9GAo4HUwjIyALLABKUqsY0KbEYONmyLiAQBFc1GK2d1nbxQ7x4pxBg5LjFCRRti9YnmWENtXVPLulCeregoDAkXumk8/q3G1lBgMzTT6O5ucFZaTqRVFWFMhaIjHsYblWNjwUwh/CzSrMbriJN4lkM8edgHAmW97SShe/c4vBVDo/WsqIws13Az90KUoKFsR6o7+tZETDTUBry/p9PQNHQDORGqrjrWuilRt5EKJOJxx+42NKR5mTKCILCCWMWEqShSkIldB3Yj07A6VNuw8Ki7EZujYE/wV2NbmuFBhwNNtluNSOxoPEIEnjyEiFCz1ezEdNeLK1+OyyHJt3mlHall2u20vOqiJIEjkH29dx3GiHGHCg+KGhzHhiVWVYDIoSrES/nU8M76VOkR9SoUi8dDXixIgRk6CZ0u5KZMqCJaJfCmk8gxNJkTdJaTdERrYlin+bCu04qgiTSjIwQQUhQ1LP3/WOK6jqGkXZRjysSNbKoRKqHaKRS3TUPNUBNFsv1nlBe9OEJ1um5KDoV6enqCPVzQdjpCLABM6JkNjQm1EyUhlZM17i1MG1nSem2ZyMtuJkWi6SUEJM6tDirKOVocOm4mgUwP1zH6jostOnH1P5LEXpIzKz1PHxMDMGUEPXYHVTsZsiE5zd5SezbiwP0IhTABKDhyNojloIOgZltmZirKk9AxrG5CKtvJjDn0zvpknOYMTEIHqqqdttyp5DiqOgkNPW3qhml6Z2cqDvPUJxOmBc2Ma6g453JaxI+lUg3azCI+cjuhRXomPdY36fjGWprXdFHloSDSnM4KSIX6tPLWULehonZjhuVupU1zWnnVou6ZUE9y1sfXVBdUTQUMrSCRfIJZMUwrbBVPs6IgibBK4xhNmVbeGzpmWikZhq8CLMOKHADitHKrMQ3SI1oyM+0Ltd7GLtKmlxq5KM6spLxeM9ZqNEZRaSCKIs0zVRodDvOIJrdZ5n5TnrCQd9P611U7NH7gjRZkf0y/7wPQ7ztGZmYQApXMCrC8pGiTv2jOYmy4KGhAPYiNDosMcg4KdqFMChpOYYlv65KjR9rzRuzBbeCR3JBdWsTMzpu4wZI7O8XM3EVlDBmWgcQyAAhMG1hxZ9fPVPgXvLn+Dd/XS99bser8dvbq2t3qmjOpLlCWE/L5igv8/b6C1psVWwbPtm3Zee3kzkXvn6iXPlv43DeB1mpcVrOXOni0adW3KxcOv7N//kc1A4dX767f8dCeD+ecXhaqBtW3bjQ0LpIG3uqb136obzjWjoLvJptW1p688ug58P25UxeG8J4rlTsWC1svHdnXwZ39g/3q8rWbh5b9+PGJ0y+8+GtpWWL5gt+YX4Z/n3ez7sBLgw8fPnY6MmvzDxHf0IWl/p9uv7r/QHmv/Vr1xU3fdbceHr7RL7WeH4B/Ky+vYo//tW/tHDt4i2l8nqr4vPQT5WqFL3T9qYvr5K5tQ3udn18pvz6rfknb65+Gb585lXq761lorq58bGhp7RPldHPX8YFUqHL+pdK56rEv/V88OVK+fwCP9dDg/BAAAA=='
+    }
   }).then(function(res) {
-    console.log(res)
+    console.log(res.data)
+    controller.items = res.data
   }, function(err) {
     console.error(err);
   })
@@ -58,7 +115,7 @@ app.controller("StoreController", ['$http', function($http) {
 
 
 app.controller("InfoController", ['$http', function($http) {
-  this.halfCent = "https://en.wikipedia.org/wiki/Half_cent_(United_States_coin)#/media/File:Half_cent_obv.jpg"
+  this.imgSrc = "https://en.wikipedia.org/wiki/Half_cent_(United_States_coin)#/media/File:Half_cent_obv.jpg"
   
   this.largeCent = "https://en.wikipedia.org/wiki/Large_cent#/media/File:1817_Large_cent_obverse.jpg"
 
@@ -98,4 +155,13 @@ app.controller("InfoController", ['$http', function($http) {
 
   this.commerative = "https://en.wikipedia.org/wiki/Modern_United_States_commemorative_coins#/media/File:86_Ellis_Island.jpg"
 
+}]);
+
+app.controller("RegisterController", ['$http', function($http) {
+  this.register = function() {
+    const makeBackendCall = $http({
+      method: 'POST',
+      url: 'http://localhost:9292/users/register'
+    })
+  }
 }]);
